342a630f88e094a28dd475efc393e80176effa27
Raul Barrera <rbarrera1992@gmail.com>
2020-10-20 12:29:33 -0500

    Filters by genres, sorts by app name or rating

--- app.js
+++ app.js
@@ -15,16 +15,44 @@ app.get('/apps', (req, res) => {
   const genresArr = ['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'];
   let results = AppsData;
 
-  if(sort && !['rating', 'app'].includes(sort)){
+  if(!genres){
+      return res.status(400).send('Genre is required as search parameter')
+  }
+
+  if(genres && !genresArr.includes(genres.toLowerCase())){
+    return res.status(400)
+      .send('Genre must be of type Action, Puzzle, Strategy, Casual, Arcade, or Card');
+  }
+
+  if(genres){
+    results = results.filter(app => app.Genres.toLowerCase().includes(genres.toLowerCase()));
+  }
+  
+  if(sort && !['rating', 'app'].includes(sort.toLowerCase())){
       return res.status(400).send('Sort must be one of rating or app');
   }
 
   if(sort){
-
-  }
-
-  if(genres){
-      results = results.filter(app => app.Genres.toLowerCase === genrees.toLowerCase);
+    let temp = sort.toLowerCase();
+    temp = sort.charAt(0).toUpperCase() + temp.slice(1);
+    
+    results.sort((a, b) => {
+      if(temp !== 'Rating'){
+        a = a[temp].toLowerCase();
+        b = b[temp].toLowerCase();
+      } else {
+        a = a[temp];
+        b = b[temp];
+      }
+      
+      if(a < b){
+        return -1;
+      } else if(a > b){
+        return 1;
+      } else {
+        return 0;
+      }
+    })
   }
 
   res.json(results)
